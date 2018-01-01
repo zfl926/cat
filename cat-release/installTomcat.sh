d60653cc45cee2ff27aff3d814ae217f210758e5
zpl <zfl926@163.com>
2018-01-01 16:00:42 +0800

    add cat-release

---
+++ cat-release/installTomcat.sh
@@ -0,0 +1,8 @@
+#install tomcat 
+
+mkdir /usr/local/tomcat
+cd /usr/local/tomcat
+wget https://mirrors.cnnic.cn/apache/tomcat/tomcat-8/v8.5.24/bin/apache-tomcat-8.5.24.tar.gz
+tar -xvf apache-tomcat-8.5.24.tar.gz 
+#startup
+#nohup ./apache-tomcat-7.0.57/bin/startup.sh
